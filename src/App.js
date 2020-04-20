7d4f9fb83893fb8a62b9a0cbfe2273faf202a82f
Daniel Mora <develop@danielmora.cr>
2020-04-20 15:46:14 -0600

    Getting breaking bad quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+
+import Quote from "./components/Quote";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -moz-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size 0.8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 
 function App() {
+  const [quote, setQuote] = useState({});
+
+  const getQuote = () => {
+    fetch("https://breaking-bad-quotes.herokuapp.com/v1/quotes")
+      .then((response) => response.json())
+      .then((content) => setQuote(content[0]));
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      <Quote quote={quote} />
+      <Button onClick={() => getQuote()}>Obtener frase</Button>
+    </Container>
   );
 }
 
